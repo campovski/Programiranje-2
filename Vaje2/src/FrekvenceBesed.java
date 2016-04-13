4323f11bafee18ca9806d93aefc796adc0b41256
campovski <campovski@gmail.com>
2016-04-13 19:08:52 +0200

    Add solutions with comments to second set of problems.

---
+++ Vaje2/src/FrekvenceBesed.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class FrekvenceBesed {
+	public static void main(String[] args) throws IOException{
+		if (args.length != 1){
+			System.out.println("Error String[] args: Too few or too many arguments (" + args.length + " arguments)!");
+			System.exit(1);
+		}
+		
+		Map<String, Integer> pojavitve = prestejPojavitve(args[0]);
+		List<String> urejeneBesede = urediKljuce(pojavitve);
+		
+		// Ker je besed ogromno, nam prav pride, ce jih shranimo v file.
+		PrintWriter izhod = new PrintWriter("prestete_besede.txt", "UTF-8");
+		
+		for (int i = 0; i < urejeneBesede.size(); i++){
+			String beseda = urejeneBesede.get(i);
+			String izpis = beseda + ": " + pojavitve.get(beseda);
+			System.out.println(izpis);
+			izhod.write(izpis + "\n");
+		}
+		izhod.close();
+	}
+	
+	// Iz vsake vrstice vhodne datoteke pobrisemo nezeljene znake, navedene v vrstici 30.
+	// Nato vsako besedo, ce je se ni v slovarju, dodamo vanj. Drugace le povecamo stevec.
+	private static Map<String, Integer> prestejPojavitve(String imeDat) throws IOException{
+		BufferedReader vhod = new BufferedReader(new FileReader(imeDat));
+		Map<String, Integer> slovarPojavitev = new HashMap<String, Integer>();
+		String line;
+		
+		while((line = vhod.readLine()) != null){
+			StringTokenizer st = new StringTokenizer(line, " .,()[]<>\"-*");
+			while (st.hasMoreTokens()){
+				String beseda = st.nextToken();
+				if (slovarPojavitev.containsKey(beseda)){
+					slovarPojavitev.put(beseda, slovarPojavitev.get(beseda)+1);
+				} else {
+					slovarPojavitev.put(beseda, 1);
+				}
+			}
+		}
+		vhod.close();
+		return slovarPojavitev;
+	}
+	
+	// Kljuce uredimo tako, da si definiramo svojo compare metodo, ki bo urejala po
+	// vrednostih (value). Ko smo uredili slovar, prepisemo kljuce v list kljuci.
+	// Opomba: Ce zelimo sortirati v obratnem vrstnem redu, zamenjamo vnos1 in vnos2
+	// v vrstici 53.
+	private static List<String> urediKljuce(Map<String, Integer> map){
+		List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(map.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
+	        public int compare(Map.Entry<String, Integer> vnos1, Map.Entry<String, Integer> vnos2)
+	        {
+	            return (vnos2.getValue()).compareTo(vnos1.getValue());
+	        }
+		});
+	    
+	    List<String> besede = new LinkedList<String>();
+	    for (int i = 0; i < list.size(); i++){
+	    	besede.add(list.get(i).getKey());
+	    }
+	    return besede;
+	}
+}
---
+++ Vaje2/src/Graf.java
@@ -0,0 +1,170 @@
+import java.util.*;
+
+public class Graf {
+	// Ustvarimo razredno spremenljivko graf.
+	public Map<Integer, Set<Integer>> graf;
+
+	// Konstruktor naredi graf.
+	public Graf(){
+		graf = new HashMap<Integer, Set<Integer>>();
+	}
+	
+	// Preverimo, da tocka, ki jo zelimo dodati, se ni v grafu.
+	public void dodajTocko(Integer v){
+		if (!graf.containsKey(v)){
+			graf.put(v, new HashSet<Integer>());
+		}
+	}
+	
+	// Povezavo dodamo tako, da najprej preverimo, ali graf vsebuje obe krajisci;
+	// ce ju, dodamo povezavo tako, da jo dodamo v obe smeri.
+	public void dodajPovezavo(Integer v, Integer u){
+		if (graf.containsKey(v) && graf.containsKey(u)){
+			graf.get(v).add(u);
+			graf.get(u).add(v);
+		}
+	}
+	
+	// Povezavo odstranimo tako, da najprej preverimo, ali sploh obstaja. Ce jo,
+	// jo nato odstranimo v obe smeri.
+	public void odstraniPovezavo(Integer v, Integer u){
+		if (graf.containsKey(v) && graf.containsKey(u) && graf.get(v).contains(u)){
+			graf.get(v).remove(u);
+			graf.get(u).remove(v);
+		}
+	}
+	
+	// Tocko odstranimo tako, da najprej preverimo, ali jo graf vsebuje.
+	// Ce jo, pogledamo najprej, s katerimi tockami je povezana. Nato odstranimo
+	// vse njene povezave in na koncu se tocko samo.
+	public void odstraniTocko(Integer v){
+		if (graf.containsKey(v)){
+			Set<Integer> kljuciOdstranitev = graf.get(v);
+			Iterator<Integer> iter = kljuciOdstranitev.iterator();
+			while (iter.hasNext()){
+				Integer kljuc = iter.next();
+				graf.get(kljuc).remove(v);
+			}
+			graf.remove(v);
+		}
+	}
+	
+	// Zapeljemo se cez urejen seznam tock in za vsako tocko preverimo, ali morda
+	// pripada kaksni ze obstojeci komponenti. Ce da, potem njo in tocke, s katerimi
+	// je povezana, dodamo v komponento. V nasprotnem vrnemo false. Ce pridemo do konca,
+	// to pomeni, da smo uspesno dodali vse tocke v eno komponento, vrnemo true.
+	public boolean povezan(){
+		List<Integer> tocke = new ArrayList<Integer>(graf.keySet());
+		Collections.sort(tocke);
+		List<Set<Integer>> komponente = new LinkedList<Set<Integer>>();
+		
+		for (int i = 0; i < tocke.size(); i++){
+			Integer tocka = tocke.get(i);
+			if (komponente.size() != 0){
+				for (int j = 0; j < komponente.size(); j++){
+					if (komponente.get(j).contains(tocka)){
+						komponente.get(j).addAll(graf.get(tocka));
+						break;
+					} else {
+						if (j + 1 == komponente.size()){
+							return false;
+						}
+					}
+				}
+			} else {
+				komponente.add(graf.get(tocka));
+			}
+		}
+		return true;
+	}
+	
+	// Zapeljemo se cez urejen seznam tock in za vsako tocko preverimo, ali morda
+	// pripada kaksni ze obstojeci komponenti. Ce da, potem njo in tocke, s katerimi
+	// je povezana, dodamo v komponento. V nasprotnem naredimo novo komponento, v
+	// katero dodamo tocko in tocke, s katerimi je povezana.
+	public int steviloKomponent(){
+		List<Integer> tocke = new ArrayList<Integer>(graf.keySet());
+		Collections.sort(tocke);
+		List<Set<Integer>> komponente = new LinkedList<Set<Integer>>();
+		
+		for (int i = 0; i < tocke.size(); i++){
+			Integer tocka = tocke.get(i);
+			if (komponente.size() != 0){
+				for (int j = 0; j < komponente.size(); j++){
+					if (komponente.get(j).contains(tocka)){
+						komponente.get(j).add(tocka);
+						komponente.get(j).addAll(graf.get(tocka));
+						break;
+					} else {
+						if (j + 1 == komponente.size()){
+							komponente.add(j+1, graf.get(tocka));
+							komponente.get(j+1).add(tocka);
+						}
+					}
+				}
+			} else {
+				komponente.add(graf.get(tocka));
+				komponente.get(0).add(tocka);
+			}
+		}
+		return komponente.size();
+	}
+	
+	// Alternativna (lepsa) resitev, ki preveri, ali je stevilo komponent enako 1.
+	public boolean povezan2(){
+		if (this.steviloKomponent2() == 1){
+			return true;
+		}
+		return false;
+	}
+	
+	// Alternativna (lepsa) resitev, ki naredi kompozicijo grafa in vrne moc kompozicije.
+	public int steviloKomponent2(){
+		return this.kompozicija().size();
+	}
+	
+	// Metoda, ki jo uporablja lepsa resitev steviloKomponent2 in posredno potem tudi
+	// povezan2. Lepsa resitev, vendar sta grsi implementirani v primeru, da lahko
+	// razred Graf vsebuje le metode, predpisane v navodilu za vaje.
+	public List<Set<Integer>> kompozicija(){
+		List<Integer> tocke = new ArrayList<Integer>(graf.keySet());
+		Collections.sort(tocke);
+		List<Set<Integer>> komponente = new LinkedList<Set<Integer>>();
+		
+		for (int i = 0; i < tocke.size(); i++){
+			Integer tocka = tocke.get(i);
+			if (komponente.size() != 0){
+				for (int j = 0; j < komponente.size(); j++){
+					if (komponente.get(j).contains(tocka)){
+						komponente.get(j).add(tocka);
+						komponente.get(j).addAll(graf.get(tocka));
+						break;
+					} else {
+						if (j + 1 == komponente.size()){
+							komponente.add(j+1, graf.get(tocka));
+							komponente.get(j+1).add(tocka);
+						}
+					}
+				}
+			} else {
+				komponente.add(graf.get(tocka));
+				komponente.get(0).add(tocka);
+			}
+		}
+		return komponente;
+	}
+	
+	// Naredimo en graf in preverimo, ali deluje... Lahko pustimo tudi prazno, ce mislimo
+	// ta razred uporabljati znotraj drugih razredov.
+	public static void main(String[] args){
+		Graf x = new Graf();
+		for (int i = 0; i < 100; i++){
+			x.dodajTocko(i);
+		}
+		for (int i = 0; i < 98; i++){
+			x.dodajPovezavo(i, i+2);
+		}
+		System.out.println(x.povezan());
+		System.out.println(x.steviloKomponent());
+	}
+}
